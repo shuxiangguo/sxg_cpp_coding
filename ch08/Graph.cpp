3d9199742c52066f7773c3983793fe86175920b3
shuxiangguo <shuxiangguo_li@163.com>
2019-12-11 21:03:49 +0800

    chapter08 图的初始化及邻接矩阵、邻接表表示和插入节点

---
+++ ch08/Graph.cpp
@@ -0,0 +1,178 @@
+//
+// Created by sxguoli on 2019/12/11.
+//
+// 图结构的实现
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+using namespace std;
+
+class Graph {
+protected:
+    int nv; // 顶点个数
+    vector<string> vertices; // 存储顶点
+    map<string, int> iov; // index of value 值的下标
+    bool directed; // 是否是有向图
+
+public:
+    Graph(bool dir=false) { // C++ 支持默认参数
+        directed = dir;
+        nv = 0;
+    }
+
+    Graph(vector<string> v, bool dir=false) {
+        directed = dir;
+        nv = (int)v.size(); // 否则会损失精度
+        vertices = v;
+        for (int i = 0; i < nv; i++) {
+            iov[v[i]] = i;
+        }
+    }
+
+    Graph(int n, bool dir=false) {
+        directed = dir;
+        nv = n;
+        vertices.resize(n);
+        for (int i = 0; i < n; i++) {
+            vertices[i] = to_string(i);
+            iov[to_string(i)] = i;
+        }
+    }
+
+    virtual void print() = 0;
+    virtual void insertV() {
+        insertV(to_string(nv));
+    }
+
+    virtual bool insertV(string v) {
+        if (iov.find(v) != iov.end()) {
+            return false;
+        } else {
+            vertices.push_back(v);
+            iov[v] = nv;
+            nv++;
+            return true;
+        }
+    }
+};
+
+
+// 邻接矩阵表示的图
+class MGraph : public Graph {
+protected:
+    vector<vector<int>> adjM;
+    void setAdjM() {
+        adjM.resize(nv);
+        for (int i = 0; i < nv; i++) {
+            adjM[i].resize(nv);
+            for (int j = 0; j < nv; j++) {
+                adjM[i][j] = INT_MAX;
+            }
+        }
+    }
+public:
+    MGraph(bool dir=false) : Graph(dir) {
+
+    }
+
+    MGraph(vector<string> v, bool dir=false) : Graph(v, dir) {
+        setAdjM();
+    }
+
+    MGraph(int n, bool dir=false) : Graph(n, dir) {
+        setAdjM();
+    }
+
+    void print() {
+        cout << "\t";
+        for (int i = 0; i < nv; i++) cout << vertices[i] << "\t";
+        cout << endl;
+        for (int i = 0; i < nv; i++) {
+            cout << vertices[i] << ":\t";
+            for (int j = 0; j < nv; j++) {
+                if (adjM[i][j] == INT_MAX) cout << "-\t";
+                else cout << adjM[i][j] << "\t";
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
+
+    void insertV() {
+        insertV(to_string(nv));
+    }
+
+    bool insertV(string v) {
+        bool r = Graph::insertV(v);
+        if (!r) return false;
+        // 给邻接矩阵增加一行一列
+        for (int i = 0; i < nv - 1; i++) {
+            adjM[i].push_back(INT_MAX);
+        }
+        adjM.push_back((vector<int>(nv, INT_MAX)));
+        return true;
+    }
+};
+
+// 邻接表表示的图
+class LGraph : public Graph {
+protected:
+    vector<map<int, int >> adjL;
+    void setAdjL() {
+        adjL.resize(nv);
+        for (auto x : adjL) {
+            x.clear();
+        }
+    }
+public:
+    LGraph(bool dir=false) : Graph(dir) {}
+    LGraph(vector<string> v, bool dir=false) : Graph(v, dir) {
+        setAdjL();
+    }
+    LGraph(int n, bool dir=false) : Graph(n, dir) {
+        setAdjL();
+    }
+
+    void print() {
+        adjL[0].insert(pair<int, int>(1, 90));
+        adjL[0].insert(pair<int,int>(2, 30));
+        for (int i = 0; i < nv; i++) {
+            cout << vertices[i] <<"[" << i << "]---->";
+            for (auto x : adjL[i]) {
+                cout << vertices[x.first] << "(" << x.second << ")  ";
+            }
+            cout << endl;
+        }
+    }
+
+    void insertV() {
+        insertV(to_string(nv));
+    }
+
+    bool insertV(string v) {
+        bool r = Graph::insertV(v);
+        if (!r) return false;
+        // 给邻接表增加一行
+        adjL.push_back(map<int, int> ());
+        return true;
+    }
+};
+
+/**
+ 0   1    2    3
+ jan Feb  Mar  Apr
+ */
+
+int main() {
+    vector<string> v = {"AA", "BB", "CC", "DD", "EE"};
+    //Graph g(v);
+//    Graph g(5);
+//    g.test();
+//    MGraph g(5);
+    LGraph g(v);
+    g.insertV("X");
+    g.print();
+    return 0;
+}
